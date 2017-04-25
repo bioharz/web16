441025b783a38dfca9a5727272cf8bf38af47660
Reza Shokri <bioharz3@gmail.com>
2017-04-25 15:56:37 +0200

    change main

--- src/main/java/fh/kufstein/Main.java
+++ src/main/java/fh/kufstein/Main.java
@@ -11,6 +11,7 @@ import org.hibernate.cfg.Configuration;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 /**
  * Created by bioharz on 11/04/17.
@@ -27,9 +28,17 @@ public class Main {
         //Student student2 = new StudentDao().addStudent("1510653033", "Reza2", "Shokri2", formatter.parse("21.01.1992"));
         //Student student3 = new StudentDao().addStudent("1510653034", "Reza3", "Shokri3", formatter.parse("21.01.1993"));
 
-        Student student1 = new StudentDao().getStudentByPKZ("1510653032");
+        //Student student1 = new StudentDao().getStudentByPKZ("1510653032");
 
-        System.out.println(student1);
+
+        //System.out.println(student1);
+
+        List<Student> studentList = new StudentDao().getAllStudents();
+
+        for (Student student: studentList
+             ) {
+            System.out.println(student);
+        }
 
         System.exit(0);
 
